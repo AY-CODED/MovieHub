559139bc0a627c7e6ee1079cafa17a184c67596f
ayomipo <ayomiposoyinka@gmail.com>
2025-06-24 10:09:00 +0100

    initial commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,14 @@
-function Footer(){
-    return(
-        <>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, iste optio soluta rerum totam voluptate vel facilis deleniti quasi, cum unde hic accusantium itaque reprehenderit quis vero. Porro, explicabo tempore.
+function Footer() {
+    return (
+        <footer className="bg-blue-950 text-white text-center p-4 mt-10">
+            <p className="text-sm">
+                © {new Date().getFullYear()} MovieHub. All rights reserved.
             </p>
-        </>
+            <p className="text-xs mt-1">
+                Built with ❤️ by MovieHub Team.
+            </p>
+        </footer>
     );
 }
 
-export default Footer
+export default Footer;
